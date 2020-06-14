cda9aa477fc0cd14e858cb393cb61b81a03438d3
Rexagon <reide740@gmail.com>
2020-06-14 17:02:47 +0300

    Add input state

---
+++ src/input/mod.rs
@@ -0,0 +1,203 @@
+mod device_input_state;
+
+use winit::dpi::PhysicalPosition;
+use winit::event::*;
+
+use self::device_input_state::*;
+
+pub struct InputState {
+    keyboard: InputStateBuffers<KeyboardState>,
+    mouse: InputStateBuffers<MouseButtonsState>,
+    mouse_position: MousePosition,
+}
+
+impl InputState {
+    pub fn new() -> Self {
+        Self {
+            keyboard: InputStateBuffers::new(),
+            mouse: InputStateBuffers::new(),
+            mouse_position: MousePosition::new(),
+        }
+    }
+
+    pub fn flush(&mut self) {
+        self.keyboard.flush();
+        self.mouse.flush();
+        self.mouse_position.flush();
+    }
+
+    pub fn handle_window_event(&mut self, event: &WindowEvent) {
+        match event {
+            WindowEvent::KeyboardInput {
+                input: KeyboardInput {
+                    virtual_keycode, state, ..
+                },
+                ..
+            } => {
+                let key = match virtual_keycode {
+                    Some(key) => key,
+                    None => return,
+                };
+
+                match state {
+                    ElementState::Pressed => self.keyboard.current.press(key),
+                    ElementState::Released => self.keyboard.current.release(key),
+                }
+            }
+            WindowEvent::MouseInput { button, state, .. } => match state {
+                ElementState::Pressed => self.mouse.current.press(button),
+                ElementState::Released => self.mouse.current.release(button),
+            },
+            WindowEvent::CursorMoved { position, .. } => {
+                self.mouse_position.handle_movement(position);
+            }
+            _ => {}
+        }
+    }
+
+    #[inline(always)]
+    pub fn keyboard(&self) -> &InputStateBuffers<KeyboardState> {
+        &self.keyboard
+    }
+
+    #[inline(always)]
+    pub fn mouse(&self) -> &InputStateBuffers<MouseButtonsState> {
+        &self.mouse
+    }
+
+    #[inline(always)]
+    pub fn mouse_position(&self) -> &MousePosition {
+        &self.mouse_position
+    }
+}
+
+pub struct InputStateBuffers<T>
+where
+    T: DeviceInputState,
+{
+    current: T,
+    previous: T,
+    any_pressed: bool,
+    any_released: bool,
+    last_pressed_key: Option<T::Key>,
+}
+
+impl<T> InputStateBuffers<T>
+where
+    T: Clone + Default + DeviceInputState,
+    T::Key: Clone,
+{
+    fn new() -> Self {
+        Default::default()
+    }
+
+    fn flush(&mut self) {
+        self.previous.clone_from(&self.current);
+        self.any_pressed = false;
+        self.any_released = false;
+        self.last_pressed_key = None;
+    }
+
+    fn handle_key(&mut self, state: &ElementState, key: &T::Key) {
+        match state {
+            ElementState::Pressed => {
+                if !self.current.is_pressed(key) {
+                    self.any_pressed = true;
+                }
+                self.current.press(key);
+                self.last_pressed_key = Some(key.clone());
+            }
+            ElementState::Released => {
+                if !self.current.is_pressed(key) {
+                    self.any_released = true;
+                }
+                self.current.release(key)
+            }
+        }
+    }
+
+    #[inline(always)]
+    pub fn last_pressed_key(&self) -> Option<T::Key> {
+        self.last_pressed_key.clone()
+    }
+
+    #[inline(always)]
+    pub fn is_pressed(&self, key: &T::Key) -> bool {
+        self.current.is_pressed(key)
+    }
+
+    #[inline(always)]
+    pub fn is_released(&self, key: &T::Key) -> bool {
+        self.current.is_pressed(key)
+    }
+
+    #[inline(always)]
+    pub fn was_pressed(&self, key: &T::Key) -> bool {
+        !self.previous.is_pressed(key) && self.current.is_pressed(key)
+    }
+
+    #[inline(always)]
+    pub fn was_released(&self, key: &T::Key) -> bool {
+        self.previous.is_pressed(key) && !self.current.is_pressed(key)
+    }
+
+    #[inline(always)]
+    pub fn was_any_pressed(&self) -> bool {
+        self.any_pressed
+    }
+
+    #[inline(always)]
+    pub fn was_any_released(&self) -> bool {
+        self.any_released
+    }
+}
+
+impl<T> Default for InputStateBuffers<T>
+where
+    T: Default + DeviceInputState,
+{
+    fn default() -> Self {
+        Self {
+            current: Default::default(),
+            previous: Default::default(),
+            any_pressed: false,
+            any_released: false,
+            last_pressed_key: None,
+        }
+    }
+}
+
+pub struct MousePosition {
+    current: PhysicalPosition<f64>,
+    previous: PhysicalPosition<f64>,
+    initialized: bool,
+}
+
+impl MousePosition {
+    pub fn new() -> Self {
+        Self {
+            current: PhysicalPosition::new(0.0, 0.0),
+            previous: PhysicalPosition::new(0.0, 0.0),
+            initialized: false,
+        }
+    }
+
+    fn flush(&mut self) {
+        self.previous = self.current;
+    }
+
+    fn handle_movement(&mut self, new_position: &PhysicalPosition<f64>) {
+        self.current = *new_position;
+        self.initialized = true;
+    }
+
+    #[inline(always)]
+    fn current(&self) -> &PhysicalPosition<f64> {
+        &self.current
+    }
+
+    #[inline(always)]
+    fn delta(&self) -> PhysicalPosition<f64> {
+        PhysicalPosition::new(self.current.x - self.previous.x, self.current.y - self.previous.y)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,56 @@
 extern crate nalgebra_glm as glm;
 
 pub mod config;
+mod input;
 mod rendering;
 
 use futures::SinkExt;
 use tokio::net::TcpStream;
 use tokio_serde::formats::SymmetricalBincode;
 use tokio_util::codec::{FramedWrite, LengthDelimitedCodec};
+use vulkano::instance::Instance;
+use vulkano_win::VkSurfaceBuild;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
 
 use embercore::*;
 
 use crate::config::Config;
+use crate::input::InputState;
 
 pub async fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
-    let socket = TcpStream::connect(config.server_address).await.unwrap();
+    let intance = {
+        let extensions = vulkano_win::required_extensions();
+        Instance::new(None, &extensions, None)?
+    };
 
-    let length_delimited = FramedWrite::new(socket, LengthDelimitedCodec::new());
-
-    let mut serialized =
-        tokio_serde::SymmetricallyFramed::new(length_delimited, SymmetricalBincode::<protocol::Message>::default());
-
-    serialized
-        .send(protocol::Message {
-            timestamp: chrono::Utc::now().timestamp(),
-            test_text: "".to_string(),
-        })
-        .await
+    let events_loop = EventLoop::new();
+    let surface = WindowBuilder::new()
+        .with_min_inner_size(LogicalSize::new(800, 600))
+        .with_inner_size(LogicalSize::new(1024, 768))
+        .with_title("embercore")
+        .build_vk_surface(&events_loop, intance.clone())
         .unwrap();
+
+    let mut input_state = InputState::new();
+
+    events_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        Event::WindowEvent { ref event, .. } => {
+            input_state.handle_window_event(event);
+        }
+        Event::RedrawEventsCleared => {
+            // TODO: draw
+        }
+        _ => {}
+    });
 
     Ok(())
 }
